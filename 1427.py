fc67d00b779dc5a40936c5f204d44a9cc0092b4f
mincheol-Jeon <alsehf0714@naver.com>
2023-08-26 11:41:38 +0900

    using sort method

--- 1427.py
+++ 1427.py
@@ -5,9 +5,6 @@ li = []
 A = int(input())
 for i in str(A):
     li.append(i)
-for i in range(len(li)):
-    swap = li.index(max(li[i:]))
-    li[swap],li[i] = li[i],li[swap]
-
+li.sort(reverse=True)
 for i in li:
     print(i)
