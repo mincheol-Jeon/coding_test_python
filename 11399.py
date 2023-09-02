3b946e78189f47f1c0c5de46b74396cbdd564819
mincheol-Jeon <alsehf0714@naver.com>
2023-09-03 01:35:12 +0900

    O(nlogn) method in O(n^2) time limit

---
+++ 11399.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+A.sort()
+
+sum = [A[i]*(N-i) for i in range(len(A))]
+
+result = 0
+for i in sum:
+    result += i
+print(result)
+print("Done")
